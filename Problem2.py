2a324969f602db3bd22eb18323251f6e6f39d2ea
Azat Sharip <60136981+AzatSharip@users.noreply.github.com>
2020-02-28 18:32:59 +0500

    Решено, но не хватает памяти компа...

--- Problem2.py
+++ Problem2.py
@@ -1,3 +1,18 @@
-1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711
 
-a = {}
+list = [1, 2, 3]
+list2 = []
+
+def list_sum(numList):
+    the_Sum = 0
+    for i in numList:
+        the_Sum = the_Sum + i
+    return the_Sum
+
+for n in range(3, 100000):
+    f = (list[n - 1]) + (list[n - 2])
+    list.append(f)
+    if f <= 100000 and f % 2 == 0:
+        list2.append(f)
+        print('List2: ', list2)
+        print('List_sum: ', list_sum(list2))
